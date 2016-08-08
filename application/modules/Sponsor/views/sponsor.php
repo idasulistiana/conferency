080865317bad95c45738085b27255e4530a80e9f
idasulistiana <idasulistiana@gmail.com>
2016-08-08 12:13:56 +0700

    sponsor, venue_hospitally page

--- application/modules/Sponsor/views/sponsor.php
+++ application/modules/Sponsor/views/sponsor.php
@@ -2,44 +2,26 @@
 	<div id="body">
 	<div class="clearfix"></div>
 		<div class="section">
-			<h2>Platinum Sponsor</h2>
+			<h2>Sponsor</h2>
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in vestibulum mi. Donec felis nunc, placerat quis varius quis, posuere sed velit. In convallis pulvinar rutrum. Suspendisse nec mi lectus, at fermentum felis.
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in vestibulum mi. Donec felis nunc, placerat quis varius quis, posuere sed velit. In convallis pulvinar rutrum. Suspendisse nec mi lectus, at fermentum felis.
 			</p>
 			<ul class="sponsor_img">
 				<li class="sponsore">
-					<img src="<?php echo base_url() ;?>assets/images/sponsor.jpg">
-				</li>
-				<li class="sponsore">
-					<img src="<?php echo base_url() ;?>assets/images/sponsor.jpg">
-				</li>
-				<li class="sponsore">
-					<img src="<?php echo base_url() ;?>assets/images/sponsor.jpg">
-				</li>
-				<li class="sponsore">
-					<img src="<?php echo base_url() ;?>assets/images/sponsor.jpg">
+					<img src="<?php echo base_url() ;?>assets/images/ikabi_logo.jpg" class="img_sponsor">
 				</li>
 			</ul>
 		</div>
 		<div class="section">
-			<h2>Gold Sponsor</h2>
+			<h2>Co Organazier</h2>
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in vestibulum mi. Donec felis nunc, placerat quis varius quis, posuere sed velit. In convallis pulvinar rutrum. Suspendisse nec mi lectus, at fermentum felis.
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in vestibulum mi. Donec felis nunc, placerat quis varius quis, posuere sed velit. In convallis pulvinar rutrum. Suspendisse nec mi lectus, at fermentum felis.
 			</p>
 			<ul class="sponsor_img">
 				<li class="sponsore">
-					<img src="<?php echo base_url() ;?>assets/images/biomass.jpg">
-				</li>
-				<li class="sponsore">
-					<img src="<?php echo base_url() ;?>assets/images/biomass.jpg">
-				</li>
-				<li class="sponsore">
-					<img src="<?php echo base_url() ;?>assets/images/biomass.jpg">
-				</li>
-				<li class="sponsore">
-					<img src="<?php echo base_url() ;?>assets/images/biomass.jpg">
+					<img src="<?php echo base_url() ;?>assets/images/Ristekdikti_logo.jpg" class="img_sponsor">
 				</li>
 			</ul>
 		</div>
--- application/modules/venue_hospitally/views/venue_hospitally.php
+++ application/modules/venue_hospitally/views/venue_hospitally.php
@@ -1,57 +1,18 @@
 
 	<div id="body">
-		<div id="content">
-			<div id="sidebar">
-				<h3>Venue And Hospitality</h3>
-				<div class="panel-group">
-				<!-- <div class="panel-hospitality"> -->
-				  <div class="panel panel-default">
-				    <div class="panel-heading">
-				      <h4 class="panel-title">
-				        <a data-toggle="collapse" href="#collapse1">Hotel</a>
-				      </h4>
-				    </div>
-				    <div id="collapse1" class="panel-collapse collapse">
-				      <div class="panel-body">Aston Bogor Hotel</div>
-				      <div class="panel-footer">Amaroossa Royal Hotel</div>
-				    </div>
-				  </div>
-				<!-- </div> -->
-				</div>
-				<div class="panel-group">
-				<!-- <div class="panel-hospitality"> -->
-				  <div class="panel panel-default">
-				    <div class="panel-heading">
-				      <h4 class="panel-title">
-				        <a data-toggle="collapse" href="#collapse2">Tours</a>
-				      </h4>
-				    </div>
-				    <div id="collapse2" class="panel-collapse collapse">
-				      <div class="panel-body">The Jungles</div>
-				      <div class="panel-footer">Kebun Raya Bogor</div>
-				    </div>
-				  <!-- </div> -->
-				</div>
-				</div>
-			</div>
-			<div id="section">
-				<h2>testing and diagnostic services</h2>
-				<p>
-					This website template has been designed by <a href="http://www.freewebsitetemplates.com/">Free Website Templates</a> for you, for free. You can replace all this text with your own text. You can remove any link to our website from this website template, you're free to use this website template without linking back to us. If you're having problems editing this website template, then don't hesitate to ask for help on the <a href="http://www.freewebsitetemplates.com/forums/">Forums</a>.
-				</p>
-				<ul>
-					<li>
-						<a href="heart-monitoring.html" class="figure"><img src="<?php echo base_url();?>assets/images/sponsor.jpg" alt=""></a>
-						<div>
-							<h3><a href="heart-monitoring.html">Heart Monitoring</a></h3>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in vestibulum mi. Donec felis nunc, placerat quis varius quis, posuere sed velit.
-							</p>
-							<a href="heart-monitoring.html" class="more">more info</a>
-						</div>
-					</li>
-				</ul>
-			</div>
+	<div class="clearfix"></div>
+		<div class="section">
+			<img src="<?php echo base_url();?>assets/images/logo_hotelsalak.png" class="img_hotelsalak">
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in vestibulum mi. Donec felis nunc, placerat quis varius quis, posuere sed velit. In convallis pulvinar rutrum. Suspendisse nec mi lectus, at fermentum felis.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in vestibulum mi. Donec felis nunc, placerat quis varius quis, posuere sed velit. In convallis pulvinar rutrum. Suspendisse nec mi lectus, at fermentum felis.
+			</p>
+			<span class="text_venue">
+				The international conference will be held:<br/>
+				On	: Monday-Tuesday, October 10-11, 2016<br/>
+				Venue	: Salak Tower Hotel,<br/>
+				Jl. Salak No. 38-40, Bogor 16121, Indonesia<br/>
+				Telepon:(0251) 7565111
+			</span>
 		</div>
 	</div>
-	
