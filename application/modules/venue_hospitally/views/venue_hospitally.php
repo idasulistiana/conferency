80b410e75e511db9cad622aa175e4225ca7b2e01
Nastiti Arum Puspawati <nastitiarump@gmail.com>
2016-08-13 18:54:45 +0700

    Perbaikan venue

--- application/modules/venue_hospitally/views/venue_hospitally.php
+++ application/modules/venue_hospitally/views/venue_hospitally.php
@@ -17,7 +17,7 @@
 				On	: Monday-Tuesday, October 10-11, 2016<br/>
 				Venue	: Salak Tower Hotel,<br/>
 				Jl. Salak No. 38-40, Bogor 16121, Indonesia<br/>
-				Telepon:(0251) 7565111
+				Phone : (0251) 7565111
 			</span>
 			<br/><br/>
 			<img src="<?php echo base_url();?>assets/images/logo_peta_hotel1.png" class="img_hotelsalak">
